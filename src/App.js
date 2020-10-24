7621e70847072bd1c9227de802f6e4ed554cd0e0
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-24 21:39:34 +0300

    Remove hard-coded state from users, add spinner, add state to app.js,

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import NavBar from "./components/layout/NavBar";
 import Users from "./components/users/Users";
 import "./App.css";
@@ -7,14 +8,22 @@ class App extends Component {
   state = {
     icon: "fab fa-github",
     title: "Github Finder",
+    users: [],
+    loading: false,
   };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
 
   render() {
     return (
       <div className="App">
         <NavBar title={this.state.title} icon={this.state.icon} />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
