2685e23b86b20bf639201e448144ef32f015513b
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-24 20:35:16 +0300

    Refactor

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class NavBar extends Component {
-  //@default values if nothing is defined on App.js
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github",
-  };
+const NavBar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary2">
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
 
-  //@Prop types to ensure the right data type is passed
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+//@default values if nothing is defined on App.js
+NavBar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
 
-  render() {
-    return (
-      <nav className="navbar bg-primary2">
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+//@Prop types to ensure the right data type is passed
+NavBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default NavBar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const UserItem = (props) => {
+const UserItem = ({ eachUser: { login, avatar_url, html_url } }) => {
   return (
     <div className="card text-center">
       <img
-        src={props.avatar_url}
-        alt="img"
+        src={avatar_url}
+        alt="avatar"
         className="round-img"
-        style={{ width: "60px" }}
-      ></img>
-      <h3>{props.login}</h3>
+        style={{ width: "70px" }}
+      />
+      <h3>{login}</h3>
+
       <div>
         <a
-          href={props.html_url}
+          href={html_url}
           className="btn btn-dark btn-sm my-1"
-          style={{ borderRadius: "4px" }}
+          style={{ borderRadius: "3px" }}
         >
           More
         </a>
       </div>
-      {props.id}
     </div>
   );
+};
+
+//@prop type
+UserItem.propTypes = {
+  eachUser: PropTypes.object.isRequired,
 };
 
 export default UserItem;
