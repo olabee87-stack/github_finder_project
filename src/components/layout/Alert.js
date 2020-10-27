8125728eb3090a29c8eecd82268942c84a8122f0
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-27 08:18:43 +0200

    Explain

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 //@{alert} - object with the message and type on App.js
+//Note - the original msg and type has been efined in Search.js and also passed on to App.js
 const Alert = ({ alert }) => {
   return (
     alert !== null && (
