7c3e5cf21e62810f7b4e022617ecee2b0749d41c
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-28 18:28:40 +0200

    Add a back button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login); //pull a username's login name- defined /user/:login on App.js
   }
+
+  //@Prop-types
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    eachUser: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
+
   render() {
     const {
       name,
@@ -21,10 +32,21 @@ class User extends Component {
     } = this.props.eachUser;
 
     const { loading } = this.props;
+
+    //@show spinner while a single user's data is loading
+    if (loading) return <Spinner />;
+
+    //@Return the below content after the single user's data has been loaded
     return (
-      <h1>
-        {name} {bio}
-      </h1>
+      <Fragment>
+        <p>{name}</p>
+        {hireable}
+        <p>{followers}</p>
+        <p>{following}</p>
+        <Link to="/" className="btn btn-light">
+          Back to Search
+        </Link>
+      </Fragment>
     );
   }
 }
